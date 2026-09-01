b548b7108aab634063a9291749774c85edd21d8f
Highstreetwebco <troth.jack@yahoo.co.uk>
2026-09-01 17:48:05 +0100

    Add lib/theme.ts

---
+++ lib/theme.ts
@@ -0,0 +1,5 @@
+export const theme = {
+  ink: '#202923', muted: '#66736A', green: '#2D6A4F', greenSoft: '#DCEDE2',
+  cream: '#F8F7F2', white: '#FFFFFF', sand: '#EFE8DA', amber: '#D8922B',
+  red: '#C94D47', line: '#E8E9E2', shadow: '#173B2918'
+};
