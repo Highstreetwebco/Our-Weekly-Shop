49d27cba017d7cdd7f0cbe6c54f9fc5f32fbf01b
Highstreetwebco <troth.jack@yahoo.co.uk>
2026-09-04 15:28:45 +0100

    Add Gemma cool-mum character avatar

--- snack/RealtimeVoiceChat.js
+++ snack/RealtimeVoiceChat.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useRef, useState } from "react";
-import { Animated } from "react-native";
-import { Image, Platform, StyleSheet, Text, TouchableOpacity, View } from "react-native";
+
+import { Animated, Image, Platform, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import { supabase } from "../lib/supabase";
 
