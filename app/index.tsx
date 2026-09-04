29c1dfcd6ba18195bc155e1709b253675c2d892f
Highstreetwebco <troth.jack@yahoo.co.uk>
2026-09-04 23:00:47 +0100

    Show supermarket signup step during account onboarding

--- app/index.tsx
+++ app/index.tsx
@@ -4,6 +4,6 @@ import type React from "react";
 
 declare const require: (path: string) => { default: React.ComponentType };
 
-const WeeklyShopApp = require("../snack/RetailerAccountBridge").default;
+const WeeklyShopApp = require("../snack/RetailerSignupOnboarding").default;
 
 export default WeeklyShopApp;
